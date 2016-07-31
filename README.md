5c6e1c85b8d6975c5758c2455ac476da4765837a
Mike <ccnp_pro@yahoo.com>
2016-07-31 00:47:55 -0400

    Add initial script

---
+++ README.md
@@ -0,0 +1,24 @@
+##Motion detector with audio prompt to hold onto a hand railing.
+
+##Written for the raspberry pi using the 3.5mm audio jack and HC-SR04 sonar sensor
+
+##Parts needed:
+* Raspberry pi
+* HC-SR04 sonar sensor
+* 10k ohm resistor
+* Speaker (for 3.5mm audio out)
+
+
+##Installation:
+* Install the 10kohm resistor between the echo pin on the HC-SR04 sensor and pi pin 20
+* Install the HC-SR04 trig pin to pi pin 21
+* Install the HC-SR04 vcc to a +5v and gnd to a ground pin
+* Connect your speaker to the pi via the 3.5mm audio jack
+* Run sonar2.py, be sure to keep the audio .ogg file in the same folder as sonar2.py
+
+
+##Running:
+* The pi will continuously scan via the sonar sensor for distance changes
+* When a large distance change is detected (like a door being opened) the audio clip will play
+* The pi will wait for ~3 seconds after playing the audio clip before resuming its sonar sensor scanning
+* The audio file can be any .ogg file, but must have the same name as the original
---
+++ sonar2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+#encoding:utf-8
+
+import RPi.GPIO as GPIO                    #Import GPIO library
+import time                                #Import time library
+import pygame
+#pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
+#pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("pleasehold1.ogg")
+pygame.mixer.music.stop()
+pygame.mixer.music.set_volume(0)
+GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering 
+
+TRIG = 21                                  #Associate pin 21 to TRIG
+ECHO = 20                                  #Associate pin 20 to ECHO
+
+print "Distance measurement in progress"
+
+GPIO.setup(TRIG,GPIO.OUT)                  #Set pin as GPIO out
+GPIO.setup(ECHO,GPIO.IN)                   #Set pin as GPIO in
+
+while True:
+
+  GPIO.output(TRIG, False)                 #Set TRIG as LOW
+  #print "Waitng For Sensor To Settle"
+  time.sleep(0.5)                            #Delay of 1/2 second
+
+  GPIO.output(TRIG, True)                  #Set TRIG as HIGH
+  time.sleep(0.00001)                      #Delay of 0.00001 seconds
+  GPIO.output(TRIG, False)                 #Set TRIG as LOW
+
+  while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
+    pulse_start = time.time()              #Saves the last known time of LOW pulse
+
+  while GPIO.input(ECHO)==1:               #Check whether the ECHO is HIGH
+    pulse_end = time.time()                #Saves the last known time of HIGH pulse 
+
+  pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
+
+  distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
+  distance = round(distance, 2)            #Round to two decimal points
+
+  if distance > 2:      #Check whether the distance is within range
+    print "Distance:",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
+    if distance > 100:
+      pygame.mixer.music.play()
+      print "Playing audio clip"
+      pygame.mixer.music.set_volume(1)
+      while pygame.mixer.music.get_busy() == True:      
+        continue
+      pygame.mixer.music.stop()
+      pygame.mixer.music.set_volume(0)
+      time.sleep(2.5)
+  else:
+    print "Out Of Range"                   #display out of range
